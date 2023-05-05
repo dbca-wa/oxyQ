8240312ecb53c2564a832a380a9136705212833e
Bart Huntley <bart.huntley@dbca.wa.gov.au>
2023-05-05 14:08:05 +0800

    bump

---
+++ R/globals.R
@@ -0,0 +1,10 @@
+globalVariables(unique(c(
+  # depth_checkR: 
+  "dif", 
+  # plotR: 
+  "DEPTH_m", "SAL_psu", "T_degC", "tsect", 
+  # site_checkR: 
+  "dd", "dup", 
+  # transect_checkR: 
+  "buf", "grp", "in_ext", "tsamp", "tsect"
+)))
